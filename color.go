22691fe7fdafeda1ffa3c3f7588e66b58215f300
scifisatan <brawlershere@gmail.com>
2025-05-28 23:06:36 +0545

    bright colors

--- color.go
+++ color.go
@@ -7,22 +7,22 @@ import (
 const (
 	Reset   = "\033[0m"
 	Black   = "\033[30m"
-	Red     = "\033[31m"
-	Green   = "\033[32m"
-	Yellow  = "\033[33m"
-	Blue    = "\033[34m"
-	Magenta = "\033[35m"
-	Cyan    = "\033[36m"
-	White   = "\033[37m"
+	Red     = "\033[91m" // Bright Red
+	Green   = "\033[92m" // Bright Green
+	Yellow  = "\033[93m" // Bright Yellow
+	Blue    = "\033[94m" // Bright Blue
+	Magenta = "\033[95m" // Bright Magenta
+	Cyan    = "\033[96m" // Bright Cyan
+	White   = "\033[97m" // Bright White
 
-	BgBlack   = "\033[40m"
-	BgRed     = "\033[41m"
-	BgGreen   = "\033[42m"
-	BgYellow  = "\033[43m"
-	BgBlue    = "\033[44m"
-	BgMagenta = "\033[45m"
-	BgCyan    = "\033[46m"
-	BgWhite   = "\033[47m"
+	BgBlack   = "\033[100m" // Bright Black background
+	BgRed     = "\033[101m" // Bright Red background
+	BgGreen   = "\033[102m" // Bright Green background
+	BgYellow  = "\033[103m" // Bright Yellow background
+	BgBlue    = "\033[104m" // Bright Blue background
+	BgMagenta = "\033[105m" // Bright Magenta background
+	BgCyan    = "\033[106m" // Bright Cyan background
+	BgWhite   = "\033[107m" // Bright White background
 )
 
 var knownTags = map[string]string{
